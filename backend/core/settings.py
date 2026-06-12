31159e55ddc7527eb863d40c30af2f75d7406e06
= <=>
2026-06-12 11:40:46 +0600

    update codes api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-a7l$f%@l6f&$ryv!!6q@$gu6cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['spaceghor.com', 'www.spaceghor.com','api.spaceghor.com']
 
 # Application definition
 
