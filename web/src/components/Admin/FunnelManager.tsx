46edbb1fd067e18f2cc97bbc5dcc86b7558af2cc
= <=>
2026-06-14 19:30:17 +0600

    update codes funnal

--- web/src/components/Admin/FunnelManager.tsx
+++ web/src/components/Admin/FunnelManager.tsx
@@ -432,7 +432,7 @@ const FunnelManager = () => {
                                         <option value="classic">Classic Minimal</option>
                                         <option value="combo">Combo Layout</option>
                                         <option value="ezymart">EyeCare Layout</option>
-                                        <option value="garden">kazir Hat Layout</option>
+                                        <option value="garden">KazirHat Layout</option>
                                         <option value="ezymart_v2">EzyMart Layout</option>
                                     </select>
                                 </div>
