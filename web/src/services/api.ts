af08d7d541fd3193567a9519a843333bbbf51cc5
= <=>
2026-06-26 15:53:32 +0600

    update codes

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const BASE_URL = 'https://api.spaceghor.com';
-export const BASE_URL = 'http://127.0.0.1:8000' 
+export const BASE_URL = 'https://api.spaceghor.com';
+// export const BASE_URL = 'http://127.0.0.1:8000' 
 
 const API_BASE_URL = `${BASE_URL}/api/`;
 
