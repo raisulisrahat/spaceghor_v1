9fb06cf3718d3feb8895cb9c5137a99b161a2142
= <=>
2026-06-29 10:53:37 +0600

    update codes

--- web/src/pages/Checkout.tsx
+++ web/src/pages/Checkout.tsx
@@ -100,7 +100,7 @@ const Checkout = () => {
 
           const firePixel = () => {
             if (typeof (window as any).fbq === 'function') {
-              (window as any)
+              (window as any).fbq('track', 'InitiateCheckout', { eventID: eventId });
             } else {
               setTimeout(firePixel, 500);
             }
